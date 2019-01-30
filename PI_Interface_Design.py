d3bb725f4e8a486a0368200c969ba5b0f336e4f0
JESVIN PALATTY <jesvinpalatty@gmail.com>
2019-01-30 20:32:03 +0530

    test

--- PI_Interface_Design.py
+++ PI_Interface_Design.py
@@ -1,5 +1,7 @@
 import os
 import time
 
-os.system("omxplayer --vol +940 Hello.mp3")
-time.sleep(1)
+os.system("omxplayer --vol +940 /home/pi/Brailler-Pi/1.mp3")
+time.sleep(0.1)
+os.system("omxplayer --vol +940 /home/pi/Brailler-Pi/2.mp3")
+time.sleep(0.1)

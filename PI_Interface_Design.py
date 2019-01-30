cf3313400304246ce4a69adc63f0a30266d00d11
JESVIN PALATTY <jesvinpalatty@gmail.com>
2019-01-30 21:06:36 +0530

    test

--- PI_Interface_Design.py
+++ PI_Interface_Design.py
@@ -1,7 +1,18 @@
 import os
 import time
+import RPi.GPIO as GPIO
 
-os.system("omxplayer --vol +940 /home/pi/Brailler-Pi/1.mp3")
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+GPIO.setup(23,GPIO.OUT)
+
+#os.system("omxplayer --vol +940 /home/pi/Brailler-Pi/1.mp3")
+#time.sleep(0.1)
+#os.system("omxplayer --vol +940 /home/pi/Brailler-Pi/2.mp3")
+#time.sleep(0.1)
+
+GPIO.output(23,1)
 time.sleep(0.1)
-os.system("omxplayer --vol +940 /home/pi/Brailler-Pi/2.mp3")
-time.sleep(0.1)
+GPIO.output(23,0)

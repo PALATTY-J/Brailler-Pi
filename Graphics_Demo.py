24bbd7c953471991c7e271e6a926b91fed22e066
JESVIN PALATTY <jesvinpalatty@gmail.com>
2019-02-14 23:46:42 +0530

    test

--- Graphics_Demo.py
+++ Graphics_Demo.py
@@ -12,18 +12,28 @@ GPIO.setup(2,GPIO.IN)
 
 ser.baudrate = 9600
 
-#os.system("omxplayer --vol +940 /home/pi/Brailler-Pi/1.mp3")
-#time.sleep(0.1)
+os.system("omxplayer --vol +940 /home/pi/Brailler-Pi/1.mp3")
+time.sleep(0.1)
 ser.write("s".encode())
 GPIO.wait_for_edge(2,GPIO.FALLING)
-#os.system("omxplayer --vol +940 /home/pi/Brailler-Pi/3.mp3")
-#time.sleep(0.1)
-#os.system("omxplayer --vol +940 /home/pi/Brailler-Pi/2.mp3")
-#time.sleep(0.1)
+os.system("omxplayer --vol +940 /home/pi/Brailler-Pi/3.mp3")
+time.sleep(0.1)
+os.system("omxplayer --vol +940 /home/pi/Brailler-Pi/2.mp3")
+time.sleep(0.1)
+os.system("omxplayer --vol +940 /home/pi/Brailler-Pi/5.mp3")
+time.sleep(0.1)
 
+
+
+y=0
+
+while(y<=20):
+    ser.write("z01".encode())
+    GPIO.wait_for_edge(2,GPIO.FALLING)
+    
 x=0
 
-while (x<=100):
+while (x<=50):
     ser.write("z01".encode())
     GPIO.wait_for_edge(2,GPIO.FALLING)
 

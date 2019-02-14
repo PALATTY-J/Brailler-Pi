10d377ec74f2daa9e8db67c48a57e9325512e175
JESVIN PALATTY <jesvinpalatty@gmail.com>
2019-02-14 14:54:56 +0530

    Update PI_Interface_Design.py

--- PI_Interface_Design.py
+++ PI_Interface_Design.py
@@ -25,14 +25,17 @@ time.sleep(0.1)
 
     
 
-ser.write("z01".encode())
-GPIO.wait_for_edge(2,GPIO.FALLING)
+while True:
+    ser.write("z01".encode())
+    GPIO.wait_for_edge(2,GPIO.FALLING)
 
-ser.write("f".encode())
-GPIO.wait_for_edge(2,GPIO.FALLING)
+    ser.write("f".encode())
+    GPIO.wait_for_edge(2,GPIO.FALLING)
 
-ser.write("x25".encode())
-GPIO.wait_for_edge(2,GPIO.FALLING)
+    ser.write("x25".encode())
+    GPIO.wait_for_edge(2,GPIO.FALLING)
+    
+
 
 #ser.write("z01".encode())
 #GPIO.wait_for_edge(2,GPIO.FALLING)

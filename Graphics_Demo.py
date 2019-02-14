2b28f0a4e03799db1311b5fb352e00b9b61a1852
JESVIN PALATTY <jesvinpalatty@gmail.com>
2019-02-15 00:12:24 +0530

    test

--- Graphics_Demo.py
+++ Graphics_Demo.py
@@ -20,6 +20,14 @@ os.system("omxplayer --vol +940 /home/pi/Brailler-Pi/3.mp3")
 time.sleep(0.1)
 os.system("omxplayer --vol +940 /home/pi/Brailler-Pi/2.mp3")
 time.sleep(0.1)
+
+os.system("omxplayer --vol +940 /home/pi/Brailler-Pi/6.mp3")
+time.sleep(0.1)
+
+ser.write("y5".encode())
+GPIO.wait_for_edge(2,GPIO.FALLING)
+
+
 os.system("omxplayer --vol +940 /home/pi/Brailler-Pi/5.mp3")
 time.sleep(0.1)
 
